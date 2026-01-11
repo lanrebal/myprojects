f6aa577733a8b95a90c898ea0688b926f2ddbc4f
lanrebal <51426887+lanrebal@users.noreply.github.com>
2026-01-10 23:11:58 -0700

    Change timer_trigger to not run on startup

--- function_app.py
+++ function_app.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+@app.timer_trigger(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def hourly_trigger_function(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info("The timer is past due!")
